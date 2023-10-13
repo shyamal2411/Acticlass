d33b2fc11fc080dde2d30048b279d6cf070c42ed
Krishna Vaibhav Yadlapalli <kr732880@dal.ca>
2023-10-13 17:03:10 -0300

    feat: added signup UI

--- Acticlass-App/src/navigation/authStack.js
+++ Acticlass-App/src/navigation/authStack.js
@@ -1,24 +1,28 @@
 import React from 'react';
-import { createStackNavigator } from '@react-navigation/stack';
+import {createStackNavigator} from '@react-navigation/stack';
 import signInScreen from '../screens/signInScreen';
-// import signUpScreen from '../screens/signUpScreen';
+import signUpScreen1 from '../screens/signUpScreen_1';
+import signUpScreen2 from '../screens/signUpScreen_2';
+import signUpScreen3 from '../screens/signUpScreen_3';
+
 // import forgotPasswordScreen from '../screens/forgotPasswordScreen';
 
 const Stack = createStackNavigator();
 
 const AuthStack = () => {
-    return (
-        <Stack.Navigator
-            screenOptions={{
-                headerShown: false
-            }}
-            initialRouteName='SignIn'
-        >
-            <Stack.Screen name="SignIn" component={signInScreen} />
-            {/* <Stack.Screen name="SignUp" component={signUpScreen} />
-            <Stack.Screen name="ForgotPassword" component={forgotPasswordScreen} /> */}
-        </Stack.Navigator>
-    )
-}
+  return (
+    <Stack.Navigator
+      screenOptions={{
+        headerShown: false,
+      }}
+      initialRouteName="SignIn">
+      <Stack.Screen name="SignIn" component={signInScreen} />
+      <Stack.Screen name="SignUp1" component={signUpScreen1} />
+      <Stack.Screen name="SignUp2" component={signUpScreen2} />
+      <Stack.Screen name="SignUp3" component={signUpScreen3} />
+      {/* <Stack.Screen name="ForgotPassword" component={forgotPasswordScreen} /> */}
+    </Stack.Navigator>
+  );
+};
 
 export default AuthStack;
