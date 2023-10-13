11f16939f86713c481f41a5cd60c84983620491a
Venkata Sreenivas Prasad Kasibhatla <vn769140@dal.ca>
2023-10-13 17:32:24 -0300

    feat: Added a page for forgot password

--- Acticlass-App/src/navigation/authStack.js
+++ Acticlass-App/src/navigation/authStack.js
@@ -1,24 +1,23 @@
 import React from 'react';
-import { createStackNavigator } from '@react-navigation/stack';
+import {createStackNavigator} from '@react-navigation/stack';
 import signInScreen from '../screens/signInScreen';
 // import signUpScreen from '../screens/signUpScreen';
-// import forgotPasswordScreen from '../screens/forgotPasswordScreen';
+import forgotPasswordScreen from '../screens/forgotPasswordScreen';
 
 const Stack = createStackNavigator();
 
 const AuthStack = () => {
-    return (
-        <Stack.Navigator
-            screenOptions={{
-                headerShown: false
-            }}
-            initialRouteName='SignIn'
-        >
-            <Stack.Screen name="SignIn" component={signInScreen} />
-            {/* <Stack.Screen name="SignUp" component={signUpScreen} />
-            <Stack.Screen name="ForgotPassword" component={forgotPasswordScreen} /> */}
-        </Stack.Navigator>
-    )
-}
+  return (
+    <Stack.Navigator
+      screenOptions={{
+        headerShown: false,
+      }}
+      initialRouteName="ForgotPassword">
+      <Stack.Screen name="SignIn" component={signInScreen} />
+      {/* <Stack.Screen name="SignUp" component={signUpScreen} /> */}
+      <Stack.Screen name="ForgotPassword" component={forgotPasswordScreen} />
+    </Stack.Navigator>
+  );
+};
 
 export default AuthStack;
---
+++ Acticlass-App/src/screens/forgotPasswordScreen.js
@@ -0,0 +1,137 @@
+import React, {useState} from 'react';
+import {
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  StyleSheet,
+  Alert,
+} from 'react-native';
+import {colors} from '../common/colors';
+// import IonIcon from 'react-native-vector-icons/Ionicons';
+import {ScrollView} from 'react-native-gesture-handler';
+
+const forgotPasswordScreen = () => {
+  const [email, Email] = useState('');
+
+  const handleSendCode = () => {
+    Alert.alert('Code sent to your registered mail!');
+    console.log('<<<<<<<<', email, '>>>>>>>>>>>>');
+
+    // handle sign in logic here
+  };
+
+  const moveToSignUp = () => {
+    // handle sign up logic here
+  };
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.appName}>Acticlass</Text>
+      <ScrollView
+        style={{
+          marginTop: 128,
+          backgroundColor: colors.secondary,
+          width: '100%',
+          borderTopLeftRadius: 50,
+          borderTopRightRadius: 50,
+        }}>
+        <View>
+          <Text style={styles.title}>Forgot Password</Text>
+          <View style={{paddingVertical: 16, paddingHorizontal: 40}}>
+            <Text style={{fontSize: 16, color: 'black', marginLeft: 10}}>
+              Email
+            </Text>
+            <TextInput
+              style={styles.input}
+              placeholderTextColor={colors.placeholder}
+              placeholder="Email"
+              onChangeText={Email}
+            />
+          </View>
+
+          <View style={{paddingVertical: 16, paddingHorizontal: 40}}>
+            <TouchableOpacity style={styles.button} onPress={handleSendCode}>
+              <Text style={styles.buttonText}>Send Code</Text>
+            </TouchableOpacity>
+          </View>
+          <View
+            style={{
+              paddingVertical: 16,
+              flexDirection: 'row',
+              alignItems: 'center',
+            }}></View>
+        </View>
+      </ScrollView>
+    </View>
+  );
+};
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: colors.primary,
+  },
+  appName: {
+    marginVertical: 52,
+    textShadowColor: 'rgba(0, 0, 0, 0.25))',
+    textShadowOffset: {width: -10, height: 10},
+    textShadowRadius: 10,
+    fontSize: 72,
+    fontWeight: 'bold',
+    color: '#CAC1C1',
+  },
+  title: {
+    alignSelf: 'center',
+    marginVertical: 24,
+    color: 'black',
+    fontSize: 34,
+    fontWeight: '500',
+  },
+  input: {
+    backgroundColor: 'white',
+    fontSize: 16,
+    color: 'black',
+    height: 48,
+    borderWidth: 1,
+    borderColor: '#ccc',
+    borderRadius: 4,
+    marginVertical: 4,
+    paddingHorizontal: 16,
+  },
+  passwordContainer: {
+    flexDirection: 'row',
+    borderWidth: 1,
+    backgroundColor: 'white',
+    borderColor: '#ccc',
+    borderRadius: 4,
+    marginVertical: 4,
+    paddingHorizontal: 10,
+    justifyContent: 'space-evenly',
+    alignItems: 'center',
+  },
+  password: {
+    width: '90%',
+    backgroundColor: 'transparent',
+    fontSize: 16,
+    color: 'black',
+    justifyContent: 'center',
+    height: 48,
+  },
+  button: {
+    backgroundColor: colors.primary,
+    width: '100%',
+    height: 48,
+    borderRadius: 4,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  buttonText: {
+    color: 'white',
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
+});
+
+export default forgotPasswordScreen;
