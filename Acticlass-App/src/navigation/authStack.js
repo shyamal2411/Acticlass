b1a2361f8b32cee7f5d1f0779d7f0a352f65a2ef
Venkata Sreenivas Prasad Kasibhatla <vn769140@dal.ca>
2023-10-15 10:33:35 -0300

    fix: Change in authStack.js

--- Acticlass-App/src/navigation/authStack.js
+++ Acticlass-App/src/navigation/authStack.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import {createStackNavigator} from '@react-navigation/stack';
 import signInScreen from '../screens/signInScreen';
-// import signUpScreen from '../screens/signUpScreen';
-import forgotPasswordScreen from '../screens/forgotPasswordScreen';
+import signUpScreen1 from '../screens/signUpScreen_1';
+import signUpScreen2 from '../screens/signUpScreen_2';
+import signUpScreen3 from '../screens/signUpScreen_3';
+
+// import forgotPasswordScreen from '../screens/forgotPasswordScreen';
 
 const Stack = createStackNavigator();
 
@@ -12,10 +15,12 @@ const AuthStack = () => {
       screenOptions={{
         headerShown: false,
       }}
-      initialRouteName="ForgotPassword">
+      initialRouteName="SignIn">
       <Stack.Screen name="SignIn" component={signInScreen} />
-      {/* <Stack.Screen name="SignUp" component={signUpScreen} /> */}
-      <Stack.Screen name="ForgotPassword" component={forgotPasswordScreen} />
+      <Stack.Screen name="SignUp1" component={signUpScreen1} />
+      <Stack.Screen name="SignUp2" component={signUpScreen2} />
+      <Stack.Screen name="SignUp3" component={signUpScreen3} />
+      {/* <Stack.Screen name="ForgotPassword" component={forgotPasswordScreen} /> */}
     </Stack.Navigator>
   );
 };
