0248a7d8f475e912826fa06624257eaafdd81519
Nisarg Sharadkumar Vaghela <ns897912@dal.ca>
2023-10-23 21:05:34 -0300

    feat: Integrated Group service with UI

--- Acticlass-App/src/common/constants.js
+++ Acticlass-App/src/common/constants.js
@@ -12,3 +12,14 @@ export const AUTH_TOKEN = "Acticlass_AUTH_TOKEN";
 export const IS_FROM_RESET = "Acticlass_AUTH_IS_FROM_RESET";
 
 export const ATTENDANCE_FREQUENCY = ['0', '15', '30', '60'];
+
+export const PubSubEvents = {
+  OnGroupCreated: 'OnGroupCreated',
+  OnGroupUpdated: 'OnGroupUpdated',
+  OnGroupDeleted: 'OnGroupDeleted',
+  OnGroupJoined: 'OnGroupJoined',
+  OnGroupLeft: 'OnGroupLeft',
+  OnGroupMemberAdded: 'OnGroupMemberAdded',
+  OnGroupMemberRemoved: 'OnGroupMemberRemoved',
+  OnGroupMemberUpdated: 'OnGroupMemberUpdated'
+}
