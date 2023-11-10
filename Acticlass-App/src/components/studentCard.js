88f5032f052b17ab2c69875a8140f3fc4741a014
Gkuldeep <kl210309@dal.ca>
2023-11-10 02:23:51 -0400

    feat: Added LeaderBoard Screen.

---
+++ Acticlass-App/src/components/studentCard.js
@@ -0,0 +1,169 @@
+import {StackActions} from '@react-navigation/native';
+import randomColor from 'randomcolor';
+import React, {useEffect} from 'react';
+import RBSheet from 'react-native-raw-bottom-sheet';
+import {createTwoButtonAlert} from './twoButtonAlert';
+import {
+  TouchableOpacity,
+  StyleSheet,
+  Text,
+  View,
+  Dimensions,
+  ScrollView,
+  Alert,
+} from 'react-native';
+import {
+  Menu,
+  MenuOption,
+  MenuOptions,
+  MenuTrigger,
+} from 'react-native-popup-menu';
+import Snackbar from 'react-native-snackbar';
+import FeatherIcon from 'react-native-vector-icons/Feather';
+import {colors} from '../common/colors';
+import {PubSubEvents, ROLES} from '../common/constants';
+import {navRef} from '../navigation/navRef';
+import authService from '../services/authService';
+import groupServices from '../services/groupServices';
+import {Console, log} from 'console';
+import EditGroup from './EditGroup';
+import {result} from 'lodash';
+
+const StudentCard = ({navigation, item}) => {
+  return (
+    <View
+      style={[
+        styles.container,
+        {
+          shadowColor: colors.placeholder,
+          shadowOffset: {width: 0, height: 8},
+          shadowOpacity: 0.5,
+          shadowRadius: 3.84,
+          elevation: 5,
+          borderRadius: 10,
+          marginHorizontal: 30,
+          backgroundColor: colors.secondary,
+          height: 72,
+          marginVertical: 16,
+        },
+      ]}>
+      <View style={styles.rectangle1}>
+        <View
+          style={{
+            width: 50,
+            height: 50,
+            marginVertical: 11,
+            marginLeft: 14,
+            flexDirection: 'column',
+            backgroundColor: randomColor({
+              luminosity: 'dark',
+              format: 'rgba',
+              alpha: 0.5,
+            }),
+            alignItems: 'center',
+            justifyContent: 'center',
+            borderRadius: 25,
+          }}>
+          <Text style={{fontSize: 16}}>{item.rank}</Text>
+        </View>
+        <View
+          style={{
+            flexDirection: 'row',
+            alignItems: 'center',
+            justifyContent: 'space-between',
+            width: '100%',
+            height: '100%',
+          }}>
+          <View
+            style={{
+              marginLeft: 14,
+              marginVertical: 16,
+              marginRight: 60,
+              flexDirection: 'column',
+            }}>
+            <Text
+              style={{fontSize: 14, fontWeight: '400', color: colors.black}}>
+              {item.StudentName}
+            </Text>
+            <Text
+              style={{
+                fontSize: 12,
+                fontWeight: '300',
+                marginTop: 12,
+                color: colors.black,
+              }}>
+              {item.EmailId}
+            </Text>
+          </View>
+          <View
+            style={{
+              display: 'flex',
+              justifyContent: 'flex-end',
+              alignItems: 'right',
+              flexDirection: 'column',
+              paddingTop: 12,
+              marginRight: 16,
+            }}>
+            <Text
+              style={{
+                fontSize: 14,
+                fontWeight: '400',
+                marginLeft: 40,
+                marginRight: 60,
+                marginBottom: 12,
+                color: colors.black,
+              }}>
+              {item.Points} points
+            </Text>
+            {authService.getRole() == ROLES.TEACHER && (
+              <TouchableOpacity onPress={() => {}}>
+                <Text
+                  style={{
+                    color: colors.danger,
+                    //marginRight: 20,
+                    marginLeft: 55,
+                    marginBottom: 11,
+                    fontSize: 14,
+                    fontWeight: '400',
+                  }}>
+                  Remove
+                </Text>
+              </TouchableOpacity>
+            )}
+          </View>
+        </View>
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+  },
+  menuText: {
+    color: colors.black,
+    fontSize: 16,
+    marginVertical: 4,
+    marginLeft: 4,
+  },
+  rectangle1: {
+    display: 'flex',
+    height: '100%',
+    maxHeight: '100%',
+    width: '100%',
+    flexDirection: 'row',
+    backgroundColor: colors.secondary,
+    borderRadius: 10,
+    //marginVertical: 10,
+    maxWidth: '100%',
+    shadowColor: colors.placeholder,
+    shadowOffset: {width: 0, height: 8},
+    shadowOpacity: 0.5,
+    shadowRadius: 3.84,
+    elevation: 5,
+  },
+});
+
+export default StudentCard;
--- Acticlass-App/src/navigation/appStack.js
+++ Acticlass-App/src/navigation/appStack.js
@@ -1,14 +1,15 @@
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { createStackNavigator } from '@react-navigation/stack';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import {createStackNavigator} from '@react-navigation/stack';
 import React from 'react';
 import FeatherIcon from 'react-native-vector-icons/Feather';
 import IonIcon from 'react-native-vector-icons/Ionicons';
-import { colors } from '../common/colors';
+import {colors} from '../common/colors';
 import ActivityScreen from '../screens/activityScreen';
 import GroupInfoScreen from '../screens/groupInfoScreen';
 import HomeScreen from '../screens/homeScreen';
 import QrCodeScanScreen from '../screens/qrCodeScanScreen';
 import SettingScreen from '../screens/settingScreen';
+import LeaderBoard from '../screens/leaderBoardScreen';
 
 const Stack = createStackNavigator();
 const Tab = createBottomTabNavigator();
@@ -32,7 +33,7 @@ const HomeTabStack = () => {
       <Tab.Screen
         name="Activity"
         options={{
-          tabBarIcon: ({ color }) => (
+          tabBarIcon: ({color}) => (
             <FeatherIcon name="activity" color={color} size={26} />
           ),
         }}
@@ -41,7 +42,7 @@ const HomeTabStack = () => {
       <Tab.Screen
         name="Home"
         options={{
-          tabBarIcon: ({ color }) => (
+          tabBarIcon: ({color}) => (
             <IonIcon name="home" color={color} size={26} />
           ),
         }}
@@ -50,7 +51,7 @@ const HomeTabStack = () => {
       <Tab.Screen
         name="Settings"
         options={{
-          tabBarIcon: ({ color }) => (
+          tabBarIcon: ({color}) => (
             <IonIcon name="settings" color={color} size={26} />
           ),
         }}
@@ -70,6 +71,7 @@ const AppStack = () => {
       <Stack.Screen name="HomeTabs" component={HomeTabStack} />
       <Stack.Screen name="GroupInfo" component={GroupInfoScreen} />
       <Stack.Screen name="QRScan" component={QrCodeScanScreen} />
+      <Stack.Screen name="LeaderBoard" component={LeaderBoard} />
     </Stack.Navigator>
   );
 };
