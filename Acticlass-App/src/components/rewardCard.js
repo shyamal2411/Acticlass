c3fd1cc2de981ded02d8b2aa9b7cd15db109386d
Gkuldeep <kl210309@dal.ca>
2023-11-19 16:02:15 -0400

    fix: Added missing RewardCard Componant.

---
+++ Acticlass-App/src/components/rewardCard.js
@@ -0,0 +1,105 @@
+import React from 'react';
+import {StyleSheet, Text, TouchableOpacity, View, Alert} from 'react-native';
+import {colors} from '../common/colors';
+import {PubSubEvents, ROLES} from '../common/constants';
+import authService from '../services/authService';
+import groupServices from '../services/groupServices';
+import Snackbar from 'react-native-snackbar';
+import {black} from 'color-name';
+
+const RewardCard = ({log}) => {
+  const isStudent = authService.getRole() == ROLES.STUDENT;
+
+  const getFormattedTime = () => {
+    dateFromTimestamp = new Date(log.timestamp);
+    const hours = dateFromTimestamp.getHours();
+    const minutes = dateFromTimestamp.getMinutes();
+    const amPM = hours >= 12 ? 'PM' : 'AM';
+    const formattedHours = hours % 12 || 12;
+    return `${formattedHours}:${minutes} ${amPM}`;
+  };
+
+  const getDescriptionForLog = () => {
+    return 'You received ' + log.points + ' Points!';
+  };
+
+  return (
+    <View style={styles.rectangle}>
+      <View
+        style={{
+          flexDirection: 'column',
+          marginTop: 10,
+          marginLeft: 16,
+          height: '100%',
+        }}>
+        <Text style={{color: colors.black, fontSize: 14, fontWeight: 600}}>
+          Rewards
+        </Text>
+        <View
+          style={{
+            marginTop: 6,
+            height: '100%',
+          }}>
+          <Text
+            style={{
+              fontSize: 12,
+              fontWeight: 400,
+              color: colors.black,
+            }}>
+            {getDescriptionForLog()}
+          </Text>
+        </View>
+      </View>
+      <View
+        style={{
+          flex: 1,
+          alignContent: 'flex-end',
+          height: '100%',
+          display: 'flex',
+          marginTop: 12,
+          marginRight: 10,
+        }}>
+        <Text
+          style={{
+            fontSize: 12,
+            fontWeight: 400,
+            textAlign: 'right',
+            color: colors.black,
+          }}>
+          {getFormattedTime()}
+        </Text>
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+  },
+  menuText: {
+    color: colors.black,
+    fontSize: 16,
+    marginVertical: 4,
+    marginLeft: 4,
+  },
+  rectangle: {
+    display: 'flex',
+    height: 56,
+    maxHeight: '100%',
+    flexDirection: 'row',
+    backgroundColor: colors.secondary,
+    borderRadius: 10,
+    marginTop: 24,
+    marginHorizontal: 8,
+    maxWidth: '100%',
+    shadowColor: colors.placeholder,
+    shadowOffset: {width: 0, height: 8},
+    shadowOpacity: 0.5,
+    shadowRadius: 3.84,
+    elevation: 5,
+  },
+});
+
+export default RewardCard;
